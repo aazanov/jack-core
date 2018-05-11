a4dda38ae7ee8f64906c7e740a3b830f8e1bca0f
Andrey <aa@stepcart.com>
2018-05-12 01:37:23 +0300

    gitignore added gulpfile mkdir

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,26 @@
-const gulp = require("gulp");
-const ts = require("gulp-typescript");
+const gulp =        require("gulp");
+const fs   =        require('fs');
+const ts =          require("gulp-typescript");
+
 const tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("default", function () {
+gulp.task("default", ['build', 'mkdir']);
+
+gulp.task("build", () => {
     return tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest("build"));
+});
+
+gulp.task("mkdir", () => {
+    const folders = [
+        'public',
+        'public/img'
+    ];
+
+    folders.forEach(dir => {
+        if(!fs.existsSync(dir))
+            fs.mkdirSync(dir),
+                console.log('📁  folder created:', dir);
+    });
 });
